43bea1ff6a77a0040247ee82d9d2fb8c08227fa0
burnsed <rburns@stic.net>
2018-11-24 20:23:30 -0800

    remove duply lock

--- backup.sh
+++ backup.sh
@@ -7,6 +7,7 @@ RUNNING=$?
 
 if [ $RUNNING -ne 0 ]; then
 	echo "Backing up"
+    rm -rf ~/.cache/duplicity/duply_heaters/lockfile.lock
 	timeout 60m duply heaters backup
 	killall -s SIGKILL -r ".*duply.*"
 	killall -s SIGKILL -r ".*duplicity.*"
