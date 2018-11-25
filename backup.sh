6770bf357be8f70ce9bea987f5d5e08f8cc115b8
burnsed <rburns@stic.net>
2018-11-24 20:15:37 -0800

    backup fix

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ RUNNING=$?
 if [ $RUNNING -ne 0 ]; then
 	echo "Backing up"
 	timeout 60m "duply heaters backup"
-	killall -r "*duply*"
-	killall -r "*duplicity*"
+	killall -s SIGKILL -r "*duply*"
+	killall -s SIGKILL -r "*duplicity*"
 else
 	echo "Duply already running"
 fi
