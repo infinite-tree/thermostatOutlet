93ff1e5543f0765b0d110779446c461c16fd8831
burnsed <rburns@stic.net>
2018-12-03 20:11:41 -0800

    Temporarily lower setpoint

--- outlet.py
+++ outlet.py
@@ -480,8 +480,13 @@ class HeatController(object):
 
         self.OutletFails = {}
 
-        self.Setpoint = config["temp_setpoint"]
-        self.Tolerance = config["temp_tolerance"]
+        # self.Setpoint = config["temp_setpoint"]
+        # self.Tolerance = config["temp_tolerance"]
+
+        # FIXME: Baby its cold outside
+        # Lowering the setpoint to conserve fuel.
+        self.Setpoint = 58.0
+        self.Tolerance = 3.0
 
     def startup(self):
         # Startup the heaters after everything has been initialized
